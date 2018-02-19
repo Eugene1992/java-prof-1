2f05885dcdf35c2c6cbc578b5a337a357eba432a
A1eksandr1 <aleeksandr.maretskij@yandex.ru>
2018-02-19 20:43:42 +0200

    Steve's commit

---
+++ steve/src/main/java/Steve_sClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Student on 2/19/2018.
+ */
+public class Steve_sClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("My");
+    }
+}
