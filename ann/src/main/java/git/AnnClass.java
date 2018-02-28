1b235aa55cf92666c8ea2f73d0b036a7c2aa2ab3
gleva <golovina.ann@gmail.com>
2018-02-28 21:50:09 +0200

    commit

--- ann/src/main/java/git/AnnClass.java
+++ ann/src/main/java/git/AnnClass.java
@@ -1,5 +1,8 @@
-package firstGIT;
+package git;
 
+/**
+ *First GIT class
+ */
 public class AnnClass {
     public static void main(String[] args) {
         System.out.println("My first maven class!");
--- ann/src/main/java/servlets/first_servlet/MyFirstServlet.java
+++ ann/src/main/java/servlets/first_servlet/MyFirstServlet.java
@@ -1,4 +1,20 @@
 package servlets.first_servlet;
 
-public class MyFirstServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello!");
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>");
+        writer.write("Hello!");
+        writer.write("</h1>");
+    }
 }
